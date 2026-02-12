bc3d4898664f745110e69132d129c996cebe0787
hendo-21 <ian.james.henderson@gmail.com>
2026-02-12 13:45:31 -0800

    Added citation to reactServer.cjs

--- frontend/reactServer.cjs
+++ frontend/reactServer.cjs
@@ -11,11 +11,22 @@ const PORT = 1886;
 // ########################################
 // ########## ROUTE HANDLERS
 
-// Handles any requests that don't match the ones above to return the React app
-// A request to '/nonExist' will redirect to the index.html where react router takes over at '/'
-// app.get('/:any*', (req, res) => {
+
+// Citation for use of AI Tools
+// Date: 02/12/26
+// Prompt(s) used: 
+//  - Why does "npm run stop_production" return "Forever cannot find process"?
+//  - What does "PathError: Missing parameter name" mean in Express 5?
+// AI Source: Google Gemini
+
+// This code below provided in the explorations was causing issues
+// app.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 // });
+
+// Handles any requests that don't match the ones above to return the React app
+// A request to '/nonExist' will redirect to the index.html where react router takes over at '/'
+// Gemini generated code snippet to resolve production server issues
 app.use((req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
