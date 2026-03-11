02b768040c8135f4caab403dc34f4659afbb5f38
hendo-21 <ian.james.henderson@gmail.com>
2026-03-11 11:09:57 -0700

    Restored ports for production build.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ import Navigation from './components/Navigation';
 
 // Define the backend port and URL for API requests
 // 1884
-const backendPort = 1890;  // Use the port you assigned to the backend server, this would normally go in a .env file
+const backendPort = 1884;  // Use the port you assigned to the backend server, this would normally go in a .env file
 const backendURL = `http://classwork.engr.oregonstate.edu:${backendPort}`;
 
 function App() {
